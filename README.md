e1557b00e49ac30816f4b119a8b4dfce9a1c4a1c
georgemathewk <georgemathewk@yahoo.com>
2021-06-03 19:22:44 +0530

    Command line arguments

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 1. hello.dart
 2. variables.dart
+3. functions.dart
+4. command_line.dart
---
+++ command_line.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {  
+  print("args.length : ${args.length}");
+  for(int i=0; i<args.length;i++){
+    print(args[i]);
+  }
+}
