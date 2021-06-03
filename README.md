0082b2f1f213ca04f05c80206868e944e843b561
georgemathewk <georgemathewk@yahoo.com>
2021-06-04 05:09:23 +0530

    Passing functions as parameters to other functions

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2. variables.dart
 3. functions.dart
 4. command_line.dart
+5. functions_as_first_class_objects.dart
---
+++ functions_as_first_class_objects.dart
@@ -0,0 +1,20 @@
+void printElement(String element) {
+  print(element);
+}
+
+void main(List<String> args) {
+  var list = ['Kerala', 'Karnataka', 'Tamilnadu', 'AndhraPradesh'];
+
+  // Passing a function as a parameter to another function
+  list.forEach(printElement);
+
+  // Assign a function to a variable
+  var printElement2 = (element) => print(element);
+  
+
+  list.forEach(printElement2);
+
+  // Passing a function body to another function
+  list.forEach((element) => print(element));
+  
+}
