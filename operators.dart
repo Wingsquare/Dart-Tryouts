251efda65c1bcaa0c95e5d83fdac655c8377a25e
georgemathewk <georgemathewk@yahoo.com>
2021-06-05 07:24:09 +0530

    Operators in Dart

--- operators.dart
+++ operators.dart
@@ -144,6 +144,63 @@ void main(List<String> args) {
     'is!': 'True if the object does not have the specified type',
   };
 
+  // Assignment operators
+  var assignmentOperators = {
+    "=": 'Assign a value',
+    "??=": 'Assign a value if the variable is null',
+    '-=': 'Subtract and Assign',
+    '/=': 'Divide and Assign',
+    '%=': 'Modulo and Assign',
+    '>>=': 'Right shift and Assign',
+    '^=': 'XOR and Assign',
+    '+=': 'Add and Assign',
+    '~/=': 'Integer division and Assign',
+    '<<=': 'Left shift and Assign',
+    '&=': 'AND and Assign',
+    '|=': 'OR and Assign',
+  };
+
+  // Logical operators
+  var logicalOperators = {
+    '!expr': 'Inverts the following expression',
+    '||': 'Logical OR',
+    '&&': 'Logical AND',
+  };
+
+  // Bitwise and Shift operators
+  var bitwiseShiftOperators = {
+    '&': 'Bitwise AND',
+    '|': 'Bitwise OR',
+    '^': 'Bitwise XOR',
+    '~expr': 'Bitwise Complement',
+    '<<': 'Shift Left',
+    '>>': 'Shift Right',
+  };
+
+  // Conditional Expressions
+  var conditionalExpressions = {
+    'condition ? expr1 : expr2':
+        'If condition is true, evalues expr1, otherwise evaluates and returns expr2',
+    'expr1 ?? expr2':
+        'If expr1 is non-null, returns its value, otherwise evaluates and returns the value of expr2',
+  };
+
+  // Cascade Notations
+  var cascadeNotations = {
+    '..': 'Allows you to make a sequence operations on the same object',
+    '?..':
+        'Allows you to make a sequence of operation on the same nullable object',
+  };
+
+  // Other Operators
+  var otherOperators = {
+    '()': 'Function Access : Represents a function call',
+    '[]':
+        'List Access : Refers to the value at the specified index in the list',
+    '.': 'Member access : ',
+    '?.': 'Conditional Member Access',
+  };
+
   // Operator Table
   print("\n***** Operator Table *****\n");
   operators.forEach((element, t) {
@@ -165,6 +222,44 @@ void main(List<String> args) {
   // Type test Operators
   print("\n***** Type test Operators *****\n");
   typeTestOperators.forEach((element, t) {
+    print('---> ' + element + ' : ' + t.toString());
+  });
+
+  // Assignment Operators
+  print("\n***** Assignment Operators *****\n");
+  assignmentOperators.forEach((element, t) {
+    print('---> ' + element + ' : ' + t.toString());
+  });
+
+  // Logical Operators
+  print("\n***** Logical Operators *****\n");
+  logicalOperators.forEach((element, t) {
+    print('---> ' + element + ' : ' + t.toString());
+  });
+
+  // Bitwise and Shift Operators
+  print("\n***** Bitwise and Shift Operators *****\n");
+  bitwiseShiftOperators.forEach((element, t) {
+    print('---> ' + element + ' : ' + t.toString());
+  });
+
+  // Conditional Expressions
+  print("\n***** Conditional Expressions *****\n");
+  conditionalExpressions.forEach((element, t) {
+    print('---> ' + element + ' : ' + t.toString());
+  });
+
+  // Cascade Notations ( This double dot notation is not an operator, this is the part of Dart Syntax)
+  print(
+      "\n***** Cascade Notations ( This is not an operator, this is the part of Dart Syntax) *****\n");
+  cascadeNotations.forEach((element, t) {
+    print('---> ' + element + ' : ' + t.toString());
+  });
+
+  // Other Operators
+  print(
+      "\n***** Other operators *****\n");
+  otherOperators.forEach((element, t) {
     print('---> ' + element + ' : ' + t.toString());
   });
 
