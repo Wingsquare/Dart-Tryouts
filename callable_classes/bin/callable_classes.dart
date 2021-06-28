b09c2c58634650609e989373c5fd958b765fd528
georgemathewk <georgemathewk@yahoo.com>
2021-06-28 19:54:05 +0530

    Callable classes

---
+++ callable_classes/bin/callable_classes.dart
@@ -0,0 +1,6 @@
+import 'package:callable_classes/callable_classes.dart' as callable_classes;
+
+void main(List<String> arguments) {
+	var f = callable_classes.MyCallableClass();
+	print(f('George','Mathew','Kunnathoor'));
+}
---
+++ callable_classes/lib/callable_classes.dart
@@ -0,0 +1,10 @@
+class MyCallableClass{
+	String call(String a, String b, String c){
+		return "$a $b $c";
+	}
+
+}
+
+void hai(){
+	print("hai");
+}
